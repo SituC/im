4d067e2ecd857b3fac4627d2cd69e2028b91c897
lwl <1181102955@qq.com>
2022-03-20 22:59:49 +0800

    feat: 完善文档

--- packages/client/src/helper/index.ts
+++ packages/client/src/helper/index.ts
@@ -1,6 +1,11 @@
 import { watch } from 'vue';
 import { useStore } from 'vuex'
 
+/**
+ * @method 实时监控登录态帮助函数
+ * @param loginCallback 登录后的回调
+ * @param noLogincallback  未登录的回调
+ */
 export const keepAliveHelper = (loginCallback: Function, noLogincallback: Function) => {
   const store = useStore()
 
